d63634b0843118cc37302ffd732754333d4c0cdc
kachrist <47902501+kachrist@users.noreply.github.com>
2019-11-17 15:52:18 -0600

    Resubmmiting

--- CSS/NAV.js
+++ CSS/NAV.js
@@ -4,7 +4,7 @@ window.onscroll = function() {myFunction()};
 // Get the navbar
 var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
+// Get the offset position of the navbar from the header
 var sticky = document.getElementById('header').offsetHeight;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
