edd2d26f5281506d2f9dbe98a127fbb02be59dc2
kachrist <47902501+kachrist@users.noreply.github.com>
2019-11-17 16:37:23 -0600

    Reworking the font system

--- CSS/NAV.js
+++ CSS/NAV.js
@@ -4,11 +4,11 @@ window.onscroll = function() {myFunction()};
 // Get the navbar
 var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar from the header
-var sticky = document.getElementById('header').offsetHeight;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
+  // Get the offset position of the navbar from the header
+  var sticky = document.getElementById('header').offsetHeight;
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
