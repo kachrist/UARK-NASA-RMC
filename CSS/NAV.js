ad0485ca9958c3e5356e0d3a2828ee0efe029b61
kachrist <47902501+kachrist@users.noreply.github.com>
2019-11-17 15:49:25 -0600

    Custom Scroll Bar
    Fixed Scolling Bug when resizing window

--- CSS/NAV.js
+++ CSS/NAV.js
@@ -5,7 +5,7 @@ window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var sticky = document.getElementById('header').offsetHeight;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
