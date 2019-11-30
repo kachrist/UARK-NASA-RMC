b88b971b6a8c7d27c6dc42938d91dd71ad2359bf
kachrist <47902501+kachrist@users.noreply.github.com>
2019-11-29 21:27:37 -0600

    Redid Slideshow to allow multiple per page

--- CSS/Slideshow.js
+++ CSS/Slideshow.js
@@ -1,28 +1,36 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"]
+var dotId = ["dot1", "dot2"]
+showSlides(1, 0);
+showSlides(1, 1);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
+
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n,no) {
+  showSlides(slideIndex[no] = n, no);
 }
 
-function showSlides(n) {
+function showSlides(n, no) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var slides = document.getElementsByClassName(slideId[no]);
+  var dots = document.getElementsByClassName(dotId[no]);
+  // cheaking end conditions
+  if (n > slides.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = slides.length}
+
+  // Clearing all images
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  // Activating only one pictures
+  slides[slideIndex[no]-1].style.display = "block";
+  dots[slideIndex[no]-1].className += " active";
 }
